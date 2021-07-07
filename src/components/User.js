a40ff5556f0a0a1c0f70fa036ca7b123170167ce
Raymond Oluoch <rasholuoch@gmail.com>
2021-07-07 20:30:55 +0300

    user info and followers components

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,14 @@ import Card from './Card'
 import Followers from './Followers'
 
 const User = () => {
-  return <h2>search component</h2>
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Card />
+        <Followers />
+      </Wrapper>
+    </section>
+  )
 }
 
 const Wrapper = styled.div`
