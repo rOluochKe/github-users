37ae97d81a52cd7aa8af185fe85110add2f523ba
Raymond Oluoch <rasholuoch@gmail.com>
2021-07-08 03:08:32 +0300

    adding user stats and charts - complete

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,45 @@ import { MdSearch } from 'react-icons/md'
 import { GithubContext } from '../context/context'
 
 const Search = () => {
-  return <h2>search component</h2>
+  const { requests, error, searchGithubUser, isLoading } =
+    React.useContext(GithubContext)
+
+  const [user, setUser] = React.useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (user) {
+      searchGithubUser(user)
+    }
+  }
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <MdSearch />
+            <input
+              type='text'
+              placeholder='enter github user'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            {requests > 0 && !isLoading && (
+              <button type='submit'>search</button>
+            )}
+          </div>
+        </form>
+        <h3>requests: {requests} / 60</h3>
+      </Wrapper>
+    </section>
+  )
 }
 
 const Wrapper = styled.div`
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,10 +4,22 @@ import loadingImage from '../images/preloader.gif'
 import { GithubContext } from '../context/context'
 
 const Dashboard = () => {
+  const { isLoading } = React.useContext(GithubContext)
+
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} className='loading-img' alt='loading' />
+      </main>
+    )
+  }
+
   return (
     <main>
-      {/* <Navbar /> */}
-      {/* <Search /> */}
+      <Navbar />
+      <Search />
       <Info />
       <User />
       <Repos />
